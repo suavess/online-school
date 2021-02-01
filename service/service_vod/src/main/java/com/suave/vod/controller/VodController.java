ed3f278eb8da32a46c515ab9028a595395032d48
suaves <1132888093@qq.com>
2021-02-01 11:28:34 +0800

    视频播放

--- service/service_vod/src/main/java/com/suave/vod/controller/VodController.java
+++ service/service_vod/src/main/java/com/suave/vod/controller/VodController.java
@@ -1,7 +1,13 @@
 package com.suave.vod.controller;
 
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthRequest;
+import com.aliyuncs.vod.model.v20170321.GetVideoPlayAuthResponse;
+import com.suave.base.exception.MyException;
 import com.suave.common.result.R;
+import com.suave.vod.constant.VodConstant;
 import com.suave.vod.service.VodService;
+import com.suave.vod.utils.VodClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,9 +34,38 @@ public class VodController {
         return R.ok().data("videoId", videoId);
     }
 
+    /**
+     * 根据视频id删除阿里云中的视频
+     *
+     * @param id
+     * @return
+     */
     @DeleteMapping("removeAliVideo/{id}")
     public R removeAliVideo(@PathVariable("id") String id) {
         vodService.removeAliVideo(id);
         return R.ok();
+    }
+
+    /**
+     * 根据视频id获取到视频播放凭证
+     *
+     * @param id
+     * @return
+     */
+    @GetMapping("getPalyAuth/{id}")
+    public R getPlayAuth(@PathVariable String id) {
+        // 创建初始化对象
+        DefaultAcsClient client = VodClientUtil.initVodClient(VodConstant.ACCESS_KEY_ID, VodConstant.ACCESS_KEY_SECRET);
+
+        GetVideoPlayAuthRequest request = new GetVideoPlayAuthRequest();
+        request.setVideoId(id);
+
+        try {
+            GetVideoPlayAuthResponse response = client.getAcsResponse(request);
+            String PlayAuth = response.getPlayAuth();
+            return R.ok().data("PlayAuth", PlayAuth);
+        } catch (Exception e) {
+            throw new MyException(20001, "获取凭证失败");
+        }
     }
 }
